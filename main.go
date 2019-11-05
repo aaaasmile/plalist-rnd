6b790ef47f5dce7e2f6329842bd57be9afc07cee
aaaasmile <testhaus@gmx.at>
2019-11-05 01:19:52 +0100

    update

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"./playlist"
 )
 
 func main() {
-	fileName := "All-title.txt" // File format is UTF 16 LE
-	maxLines := -1              // -1 are all items, > 0 is a value that cuts the export file
+	var inputFile = flag.String("in", "All-title.txt", "Playlist with all songs as input (itunes export)")
+	var outFile = flag.String("out", "Rnd-Grigio.txt", "Shuffled playlist output (itunes import)")
+
+	flag.Parse()
+
+	//fileName := "All-title.txt" // File format is UTF 16 LE
+	maxLines := -1 // -1 are all items, > 0 is a value that cuts the export file
 	pl := playlist.PlaylistRnd{}
 
-	pl.ReadFile(fileName)
+	pl.ReadFile(*inputFile)
 	//arr := []int{36, 37} // Export only this indexes
 	//pl.SetFinalIx(arr)
 
@@ -19,7 +25,7 @@ func main() {
 
 	pl.ShuffleFinalIx()
 
-	pl.WriteFile("Randomized.txt", maxLines)
+	pl.WriteFile(*outFile, maxLines)
 
 	log.Println("That's all folks!")
 }
